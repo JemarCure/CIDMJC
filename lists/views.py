22b93430c3ffff3dda829e6c99e8d43fd4738b99
Jemar <nature7192@gmail.comm>
2018-10-16 19:38:26 -0700

    A Separate Template for Viewing Lists

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,18 @@ from lists.models import Item
 from django.shortcuts import redirect, render
 from lists.models import Item
 
-def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
 
+def home_page(request):
+        if request.method == 'POST':
+         Item.objects.create(text=request.POST['item_text'])
+         return redirect('/lists/the-only-list-in-the-world/')
+    
+        return render(request, 'home.html')
+
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
+
+    
